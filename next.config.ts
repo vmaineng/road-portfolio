945a5d8998590868b5bc298e7ac7b1346b62e0c4
Mai Vang <vmaineng@gmail.com>
2025-05-13 18:55:26 -0700

    added in build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'mapbox-gl': 'maplibre-gl' 
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
