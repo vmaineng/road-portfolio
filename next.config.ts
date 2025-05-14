ee6ca7ca020d4a80eb2a2686514542a1a50d16df
Mai Vang <vmaineng@gmail.com>
2025-05-14 11:49:08 -0700

    added in async

--- next.config.ts
+++ next.config.ts
@@ -14,22 +14,20 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' api.mapbox.com",
-              "style-src 'self' 'unsafe-inline'",
-              "connect-src 'self' api.mapbox.com events.mapbox.com",
-              `img-src 'self' data: blob: *.mapbox.com`,
-            ].join('; ')
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS'
           }
         ]
       }
     ]
-  },
+  }
 };
 
 export default nextConfig;
