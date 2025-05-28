3b1468fc6732fe7034ad65807aeb28a136d7915d
Mai Vang <vmaineng@gmail.com>
2025-05-28 09:52:26 -0700

    added in more info

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -25,15 +25,20 @@ const About: React.FC = () => {
           was working on automating reports.
         </p>
         <p className="text-lg mb-6">
-          I specialize in front-end development using technologies like React,
-          Next.js, and of course, the styling magic of Tailwind CSS. I&apos;m
-          also constantly exploring new tools and techniques to expand my
-          skillset and stay at the forefront of web development trends.
+          I specialize in{" "}
+          <strong>
+            implementing functionalities between front-end and back-end{" "}
+          </strong>{" "}
+          using technologies like React and Next.js. For example, grabbing a
+          button from the UI and adding life to it so it can submit information
+          to be captured in the backend. I&apos;m also constantly exploring new
+          tools and techniques to expand my skillset, such as V0, Bolt, Remix,
+          and so many more.
         </p>
         <p className="text-lg mb-6">
-          Beyond the code, I Love eating, exploring, and trying new activities.
-          I believe that creativity and problem-solving skills extend beyond the
-          digital realm.
+          Beyond coding, I Love eating, exploring, and trying new activities.
+          One of my biggest goal is to eat through all of LA and I am sure I
+          barely made a dent.
         </p>
         <p className="text-lg">
           Thanks for stopping by on my portfolio road trip! I&apos;m excited to
